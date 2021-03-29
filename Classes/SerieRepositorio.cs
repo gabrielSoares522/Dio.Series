b63608924ff972f01d61c4368ab882130437962e
gabriel soares <gabrielcsoares52@gmail.com>
2021-03-29 00:46:20 -0300

    aula 02 implementando interface

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using Dio.Series.Interface;
+
+namespace Dio.Series.Classes
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        public void Atualiza(int id, Serie entidade)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Inserir(Serie entidade)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Interface/IRepositorio.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace Dio.Series.Interface
+{
+    public interface IRepositorio<T>
+    {
+         List<T> Lista();
+
+         T RetornaPorId(int id);
+
+         void Inserir(T entidade);
+
+        void Excluir(int id);
+
+        void Atualiza(int id,T entidade);
+
+        int ProximoId();
+    }
+}
